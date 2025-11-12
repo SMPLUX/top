8ea89a9fbb21e86993581f5b2f65e1fa508e1ba4
Erik Kijk in de Vegte <erikkijkindevegte@valori.nl>
2025-11-12 21:18:23 +0100

    fix expected fail

--- tests/1. login.spec.ts
+++ tests/1. login.spec.ts
@@ -6,24 +6,24 @@ test('login with standard user', async ({ page, loginPage }) => {
   await loginPage.login(userData);
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
   await expect(page.locator('.inventory_list')).toBeVisible();
-    expect(page.locator('[data-test="inventory-item-sauce-labs-backpack-img"]')).toBeVisible();
+  await expect(page.locator('[data-test="inventory-item-sauce-labs-backpack-img"]')).toBeVisible();
 
 });
 
 test('login with lockedout user', async ({ page, loginPage }) => {
   const userData = UserDataBuilder.withDefault()
-  .username('locked_out_user')
-  .password('secret_sauce')
-  .build();
+    .username('locked_out_user')
+    .password('secret_sauce')
+    .build();
   await loginPage.login(userData);
   await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
 
 test.fail('login with problem user', async ({ page, loginPage }) => {
   const userData = UserDataBuilder.withDefault()
-  .username('problem_user')
-  .password('secret_sauce')
-  .build();
+    .username('problem_user')
+    .password('secret_sauce')
+    .build();
   await loginPage.login(userData);
-  expect(page.locator('[data-test="inventory-item-sauce-labs-backpack-img"]')).toBeVisible();
+  await expect(page.locator('[data-test="inventory-item-sauce-labs-backpack-img"]')).toHaveValues({ src: '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg' });
 });
